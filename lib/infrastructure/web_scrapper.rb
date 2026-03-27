d9c384df6205dc2486947a4df9d00b54b78de246
MartinM1312 <murriaguimartin@gmail.com>
2026-03-27 00:23:38 -0500

    Webscrapper extracting data successfully

--- lib/infrastructure/web_scrapper.rb
+++ lib/infrastructure/web_scrapper.rb
@@ -1,19 +1,31 @@
 require 'nokogiri'
 require 'open-uri'
-
+require_relative '../domain/entry'
 class WebScrapper
-  def initialize
-    base_url = 'https://news.ycombinator.com/'
-    
-    html_content = URI.open(base_url)
+  def initialize(url = 'https://news.ycombinator.com/' )
+    @url = url
+  end
+
+  def fetch_entries
+    html_content = URI.open(@url)
 
     doc = Nokogiri::HTML(html_content)
 
-    entries = doc.css('.athing')
+    entries = []
 
-    p '******* Entries ************'
-    p entries.count
+    doc.css('.athing').each do |entry| 
+      rank = entry.css('.rank').text.to_i
+      title = entry.css('.titleline').text
+
+      entry_subtext = entry.next_element
+      score = entry_subtext.css('.score').text.to_i
+      comments = entry_subtext.element_children.text.split('|').last.strip.to_i
+
+      entries << Entry.new(rank: rank, title: title, points: score, comments: comments)
+    end
+
+    entries
+    
   end
-end
 
-scrape = WebScrapper.new
+end
